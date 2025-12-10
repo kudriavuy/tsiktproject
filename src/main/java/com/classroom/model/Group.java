4790cc746c4a0e2804c386e501e5da5509adde9e
kudriavuy <kudriavuy20@gmail.com>
2025-12-10 05:48:27 +0100

    Group added

---
+++ src/main/java/com/classroom/model/Group.java
@@ -0,0 +1,37 @@
+package com.classroom.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "groups")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Group {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "group_id")
+    private Long groupId;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    @Column(name = "created_by", nullable = false)
+    private Long createdBy;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+}
+

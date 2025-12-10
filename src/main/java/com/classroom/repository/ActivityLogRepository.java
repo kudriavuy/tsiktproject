9f03f2a2e61491470660b367a7da0d455c633449
kudriavuy <kudriavuy20@gmail.com>
2025-12-10 05:41:52 +0100

    ActivityLogRepository added

---
+++ src/main/java/com/classroom/repository/ActivityLogRepository.java
@@ -0,0 +1,13 @@
+package com.classroom.repository;
+
+import com.classroom.model.ActivityLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ActivityLogRepository extends JpaRepository<ActivityLog, Long> {
+    List<ActivityLog> findByUserIdOrderByTimestampDesc(Long userId);
+}
+

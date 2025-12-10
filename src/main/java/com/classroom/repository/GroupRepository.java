a32b83cc2c281bff4a931a8bd2b9c050ddaa94d7
kudriavuy <kudriavuy20@gmail.com>
2025-12-10 05:40:35 +0100

    GroupRepository added

---
+++ src/main/java/com/classroom/repository/GroupRepository.java
@@ -0,0 +1,13 @@
+package com.classroom.repository;
+
+import com.classroom.model.Group;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface GroupRepository extends JpaRepository<Group, Long> {
+    List<Group> findByCreatedBy(Long createdBy);
+}
+

a1ff727fb037bafaf27a8856ccdc6a6ead33a7be
kudriavuy <kudriavuy20@gmail.com>
2025-12-10 05:24:18 +0100

    WebSocketConfig added

---
+++ src/main/java/com/classroom/websocket/WebSocketConfig.java
@@ -0,0 +1,5 @@
+package com.classroom.websocket;
+
+// STOMP configuration removed, as JavaFX client uses regular WebSocket
+// If STOMP is needed for web clients, a separate configuration can be added
+
